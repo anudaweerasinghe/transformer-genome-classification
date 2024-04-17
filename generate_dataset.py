43733b68f8f7923269a87222fe0a2d666230dce1
addykan <addykan@gmail.com>
2024-04-17 18:55:59 -0400

    Allow uneven label counts, truncate all sequences to 1k, fix bugs in preprocessing

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,21 +1,25 @@
-import random, csv, os
+import random, csv, os, statistics
 
-COVID_PATH = "./data/species/hiv"
+COVID_PATH = "./data/species/covid"
 HIV_PATH = "./data/species/hiv"
-INFLUENZA_PATH = "./data/species/hiv"
+INFLUENZA_PATH = "./data/species/influenza"
 TRAIN_CSV_PATH = "train.csv"
 TEST_CSV_PATH = "test.csv"
 TRAIN_TEST_SPLIT_RATIO = 0.8
-SEQ_LENGTH = 8000
+SEQ_LENGTH = 1000
 
 
 def get_data(fasta_files_dir, label):
     data = []
+    seqs = []
     for file in os.listdir(fasta_files_dir):
         with open(os.path.join(fasta_files_dir, file), "r") as f:
             lines = f.readlines()
             sequence = lines[1]
             data.append((sequence, label))
+            seqs.append(sequence)
+    print("Median seq length:", statistics.median([len(seq) for seq in seqs]))
+    print("Mean seq length:", statistics.mean([len(seq) for seq in seqs]))
     return data
 
 
@@ -23,8 +27,8 @@ def truncate_sequences(data, length):
     # Truncate sequences to 8k nucleotides
     for i in range(len(data)):
         seq, label = data[i]
-        startIndex = random.randint(0, len(seq) - 8000)
-        data[i] = (seq[startIndex : startIndex + 8000], label)
+        startIndex = random.randint(0, len(seq) - length)
+        data[i] = (seq[startIndex : startIndex + length], label)
 
 
 def write_to_csv(csv_name, data):
@@ -54,23 +58,23 @@ def main():
 
     # Collect raw data
     hiv_data = get_data(HIV_PATH, "H")
-    influenza_data = get_data(INFLUENZA_PATH, "H")
-    covid_data = get_data(COVID_PATH, "H")
+    print("HIV data count: ", len(hiv_data))  # 20525
+    influenza_data = get_data(INFLUENZA_PATH, "I")
+    print("Influenza data count: ", len(influenza_data))  # 
+    covid_data = get_data(COVID_PATH, "C")
+    print("COVID data count: ", len(covid_data))  # 20525
 
     print("Collected raw data")
 
     # Truncate such that every dataset has the same number of sequences
     min_data_count = min(len(hiv_data), len(influenza_data), len(covid_data))
-    print("HIV data count: ", len(hiv_data))  # 20525
-    print("Influenza data count: ", len(influenza_data))  # 20525
-    print("COVID data count: ", len(covid_data))  # 20525
     print("Min data count: ", min_data_count) # 20525
     random.shuffle(hiv_data)
     random.shuffle(influenza_data)
     random.shuffle(covid_data)
-    hiv_data = hiv_data[:min_data_count]
-    influenza_data = influenza_data[:min_data_count]
-    covid_data = covid_data[:min_data_count]
+    # hiv_data = hiv_data[:min_data_count]
+    # influenza_data = influenza_data[:min_data_count]
+    # covid_data = covid_data[:min_data_count]
 
     # Split data into train and test
     hiv_train_data, hiv_test_data = split_dataset(hiv_data, TRAIN_TEST_SPLIT_RATIO)
@@ -87,13 +91,33 @@ def main():
     train_data.extend(hiv_train_data)
     train_data.extend(influenza_train_data)
     train_data.extend(covid_train_data)
+
     test_data.extend(hiv_test_data)
     test_data.extend(influenza_test_data)
     test_data.extend(covid_test_data)
 
-    # Truncate sequences to 8k nucleotides
+    # Truncate to correct length
+    # min_train_seq_length = min([len(seq) for seq, label in train_data])
+    # min_test_seq_length = min([len(seq) for seq, label in test_data])
+    # min_seq_length = min(min_train_seq_length, min_test_seq_length)
+    print("Removing sequences shorter than 1k nucleotides")
+    train_data = list(filter(lambda x: len(x[0]) >= SEQ_LENGTH, train_data))
+    test_data = list(filter(lambda x: len(x[0]) >= SEQ_LENGTH, test_data))
     truncate_sequences(train_data, SEQ_LENGTH)
     truncate_sequences(test_data, SEQ_LENGTH)
+
+    hiv_train_count = sum([1 for _, label in train_data if label == "H"])
+    hiv_test_count = sum([1 for _, label in test_data if label == "H"])
+    influenza_train_count = sum([1 for _, label in train_data if label == "I"])
+    influenza_test_count = sum([1 for _, label in test_data if label == "I"])
+    covid_train_count = sum([1 for _, label in train_data if label == "C"])
+    covid_test_count = sum([1 for _, label in test_data if label == "C"])
+    print("HIV train count: ", hiv_train_count)
+    print("HIV test count: ", hiv_test_count)
+    print("Influenza train count: ", influenza_train_count)
+    print("Influenza test count: ", influenza_test_count)
+    print("COVID train count: ", covid_train_count)
+    print("COVID test count: ", covid_test_count)
 
     print("Writing to CSV")
     # Write to CSV
