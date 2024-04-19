8a1ae97d854d93e2bd32bac6acc3b8242afc5163
Tarun Bandi <tabandi2004@gmail.com>
2024-04-19 00:39:45 -0400

    Create the fasta file script

---
+++ create-fasta.py
@@ -0,0 +1,15 @@
+from datasets import load_dataset
+
+print("hello, world")
+
+dataset = load_dataset("anudaw/genome-classification")
+train_data = dataset['train']
+test_data = dataset['test']
+
+with open("blastdb/db.fasta", "w") as fasta_file:
+    for i in range(48000):
+        fasta_file.write(">" + train_data[i]['label'] + f"{i}" + "\n")
+
+        for i in range(0, 1000, 60):
+            fasta_file.write(train_data[i]['seq'][i:i+60] + "\n")
+
